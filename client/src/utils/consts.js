de866e8dcc6595effc5b0e1103913aa61df49422
sadFrog-guy <adikokey@mail.ru>
2022-11-02 19:36:11 +0600

    init

--- client/src/utils/consts.js
+++ client/src/utils/consts.js
@@ -1,6 +1,15 @@
+import {useContext} from "@types/react";
+import {Context} from "./context";
+
 export const tgWebApp = window.Telegram.WebApp
 export const tgUser = tgWebApp.initDataUnsafe?.user
 export const tgID = tgUser.id
+
+const {Theme} = useContext(Context);
+
+tgWebApp.onEvent('themeChanged', () => {
+    Theme.setCurrentTheme()
+})
 
 export function openLinkExternal(link) {
     tgWebApp.openLink(link)
