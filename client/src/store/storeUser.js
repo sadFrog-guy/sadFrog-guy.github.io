8b6fe544c82328c0aec8445ec7014e53ac0bafb2
sadFrog-guy <adikokey@mail.ru>
2022-11-14 07:45:11 +0600

    init

--- client/src/store/storeUser.js
+++ client/src/store/storeUser.js
@@ -21,7 +21,7 @@ export default new class StoreUser {
     async getUserInfo() {
         try {
             const {data} = await User.getUser()
-            console.log('https://crypto-learn.ru/api/general_info?telegram_id' + tgID + '&hash_key=' + tgHash)
+            console.log(data)
 
             runInAction(() => {
                 this.user = data
