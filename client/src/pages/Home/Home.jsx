c405f0f28f65ac29974df7178ecac4780fccf2a0
sadFrog-guy <adikokey@mail.ru>
2022-11-14 07:30:20 +0600

    init

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -44,7 +44,7 @@ const Home = () => {
         }
     }, [isLoaded])
 
-    console.log(JSON.parse(decodeURIComponent(tgHash)))
+    console.log(JSON.parse(decodeURIComponent(tgHash).replace(/=([^&]+)\&/g, "=\"$1\",\n\r").replace(/(^|\r)([^=]+)=/g, "\"$2\":").replace(/^/, '{').replace(/$/, '}')))
 
     return (
         <Wrapper>
