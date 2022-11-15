b552e56fe2febdf5c4b0ac141ca9230d62ee3cdc
sadFrog-guy <adikokey@mail.ru>
2022-11-16 01:20:20 +0600

    init

--- client/src/pages/Training/TrainingDetail.jsx
+++ client/src/pages/Training/TrainingDetail.jsx
@@ -108,7 +108,6 @@ const TrainingDetail = ({id}) => {
 
         return () => {
             tgMainButton.hide()
-            backButtonHide()
             window.removeEventListener('scroll', handleScroll)
         }
     }, [])
