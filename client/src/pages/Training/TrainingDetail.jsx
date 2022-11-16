320c68d0df0a32b3042c90a4e09927ad7c5078fe
sadFrog-guy <adikokey@mail.ru>
2022-11-16 23:43:48 +0600

    init

--- client/src/pages/Training/TrainingDetail.jsx
+++ client/src/pages/Training/TrainingDetail.jsx
@@ -103,7 +103,7 @@ const TrainingDetail = ({id}) => {
     }, [])
 
     // useEffect(() => {
-        console.log(videoRef.current.poster)
+        console.log(videoRef.current.getAttribute("poster"))
 
     //     if(videoRef.current.poster) {
     //         setLoaded(true)
