bcb71ad557ac50df26ed69c17d08458c4b2bbe82
sadFrog-guy <adikokey@mail.ru>
2022-11-18 01:16:57 +0600

    init

--- client/src/pages/Training/TrainingFrame.jsx
+++ client/src/pages/Training/TrainingFrame.jsx
@@ -18,9 +18,9 @@ const TrainingFrame = () => {
 
                 <iframe
                     frameborder="0"
-                    style="overflow:hidden;overflow-x:hidden;overflow-y:hidden;height:150%;width:150%;position:absolute;top:0px;left:0px;right:0px;bottom:0px"
+                    className="iframe-wrap"
                     height="150%"
-                    width="150%"
+                    width="100%"
                     src={`https://${window.location.hostname}/trainings/frame/${id}`}
                 />
             </Wrap>
