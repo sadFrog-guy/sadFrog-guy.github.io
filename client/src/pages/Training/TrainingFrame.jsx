dec7356632771d21f327ec4d980c6e9f0f24351e
sadFrog-guy <adikokey@mail.ru>
2022-11-18 00:54:15 +0600

    init

--- client/src/pages/Training/TrainingFrame.jsx
+++ client/src/pages/Training/TrainingFrame.jsx
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import {useParams} from "react-router-dom";
 import Navigation from "../../components/ui/GlobalUI/Navigation/Navigation";
 import Wrapper from "../../components/utils/Wrapper/Wrapper";
 import Wrap from "../../components/utils/Wrap/Wrap";
+import {Context} from "../../utils/context";
 
 const TrainingFrame = () => {
     const {id} = useParams()
+    const {Trainings} = useContext(Context)
+
+    Trainings.getOneTraining(id)
 
     return (
         <Wrapper>
@@ -14,7 +18,7 @@ const TrainingFrame = () => {
                     Обучение
                 </Navigation>
 
-                <iframe src={`https://${window.location.hostname}/trainings/frame/${id.id}`}/>
+                {/*<iframe src={`https://${window.location.hostname}/trainings/frame/${id.id}`}/>*/}
             </Wrap>
         </Wrapper>
     );
