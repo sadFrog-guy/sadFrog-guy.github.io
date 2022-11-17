2806dcbbfdd6655bcf03f0335bfb35c4a7013f4d
sadFrog-guy <adikokey@mail.ru>
2022-11-18 02:43:52 +0600

    init

--- client/src/components/ui/GlobalUI/IFrame/IFrame.jsx
+++ client/src/components/ui/GlobalUI/IFrame/IFrame.jsx
@@ -2,18 +2,9 @@ import React from 'react';
 import {useRef, useState} from "react";
 
 const IFrame = ({...props}) => {
-    const frameRef = useRef(null)
-    const [height, setHeight] = useState("0px")
-
-    const onLoad = () => {
-        setHeight(frameRef.current.contentWindow.document.body.scrollHeight + "px");
-    }
-
     return (
         <iframe
             {...props}
-            ref={frameRef}
-            onLoad={onLoad}
             frameborder="0"
             width="100%"
         />
