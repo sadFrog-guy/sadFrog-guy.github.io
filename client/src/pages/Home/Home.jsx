3b961053c2e0a7442870940d8a70aac355cea203
sadFrog-guy <adikokey@mail.ru>
2022-11-23 12:47:50 +0600

    init

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -57,16 +57,33 @@ const Home = () => {
     }, [])
 
     useLayoutEffect(() => {
-        async function fetchOnce() {
+        const ready = function ( fn ) {
+
+            // Sanity check
+            if ( typeof fn !== 'function' ) return;
+
+            // If document is already loaded, run method
+            if ( document.readyState === 'complete'  ) {
+                return fn();
+            }
+
+            // Otherwise, wait until document is loaded
+            // The document has finished loading and the document has been parsed but sub-resources such as images, stylesheets and frames are still loading. The state indicates that the DOMContentLoaded event has been fired.
+            // document.addEventListener( 'interactive', fn, false );
+
+            // Alternative: The document and all sub-resources have finished loading. The state indicates that the load event has been fired.
+            document.addEventListener( 'complete', fn, false );
+
+        };
+
+        ready(async function() {
             setLoading(true)
             await Security.postHashKey()
             await User.getUserInfo()
             avatarLoader()
             subscribeTimer(User.subscribe_expire_datetime, User.subscription_name);
             setLoading(false)
-        }
-
-        fetchOnce()
+        });
     }, [])
 
     return (
