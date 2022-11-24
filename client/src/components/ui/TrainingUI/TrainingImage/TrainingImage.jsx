b51134cba15b4e556a0400599bdce1a24c49e11c
sadFrog-guy <adikokey@mail.ru>
2022-11-24 18:36:31 +0600

    init

--- client/src/components/ui/TrainingUI/TrainingImage/TrainingImage.jsx
+++ client/src/components/ui/TrainingUI/TrainingImage/TrainingImage.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import {useState} from "react";
 
-const TrainingImage = ({src}) => {
+const TrainingImage = ({src, onLoad}) => {
     return (
         <div className="training-image-wrap">
             <img
                 width="375"
                 height="212"
                 src={src}
+                onLoad={onLoad}
                 alt=""
                 className="training-image"
             />
