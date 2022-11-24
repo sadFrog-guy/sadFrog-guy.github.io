0dfcac93e1d27a3714c227861e8710a1d12205e9
sadFrog-guy <adikokey@mail.ru>
2022-11-25 03:21:50 +0600

    init

--- client/src/components/ui/CalculatorUI/CalculateForm/CalculateForm.jsx
+++ client/src/components/ui/CalculatorUI/CalculateForm/CalculateForm.jsx
@@ -44,9 +44,9 @@ const CalculateForm = () => {
     }
 
     const intervalDelayUpdate = async() => {
-        clearInterval(intervalId.current)
+        // clearInterval(intervalId.current)
         if(Calculator.auto_update && !Calculator.error) await Calculator.getChains(Calculator.amount)
-        setInterval(intervalId.current, Calculator.autoupdate_delay * 1000)
+        // setInterval(intervalId.current, Calculator.autoupdate_delay * 1000)
     }
 
     useEffect(() => {
